dcfc307801f23e6ee77d1a8de513913ff26a0773
evan-zahreichuk <evanzahreichuk@gmail.com>
2023-02-21 18:26:17 +0200

    Lab_01

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) {
+        char inChar;
+        System.out.println("Input symbol");
+        try {
+            inChar = (char) System.in.read();
+            System.out.println("Your symbol is " + inChar);
+        } catch (IOException e) {
+            System.out.println("Input ERROR");
+        }
+    }
+}
